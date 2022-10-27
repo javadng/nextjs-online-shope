7279ef81172f915c31a44682f4f55a64cf817e16
javadng <javadnasirifu@gmail.com>
2022-10-27 21:44:41 +0330

    master: test3

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -3,17 +3,15 @@ import classes from "../../styles/SingleProduct.module.scss";
 
 import ProductDetail from "../../components/Product/SingleProduct/ProductDetail";
 import LoadingSpinner from "../../components/UI/spinners/LoadingSpinner";
-import ProductRelated from "../../Components/Product/SingleProduct/ProductRelated";
 import SectionTitle from "../../components/UI/SectionTitle";
 
 const SingleProduct = props => {
   const { productData, error } = props;
   let pageContent;
 
-  if (productData) {
+  if (productData || !error) {
     pageContent = (
       <div className={classes.single__product}>
-        <ProductRelated className={classes.related} />
         <ProductDetail
           productDetail={productData}
           className={classes.detaile}
